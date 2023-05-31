7ae36f6155858bda93800f58fa6402ad94fc0272
akjagadish <akshaykjagadish@gmail.com>
2023-05-31 16:48:38 +0200

    updating prompt for instructGPT type to vanillaLLama type model

--- categorisation/query.py
+++ categorisation/query.py
@@ -25,7 +25,8 @@ def act(text=None, run_gpt=False, action_letters=['1', '2']):
         #     max_tokens = 1,
         #     temperature = 0.0,
         # )
-        raw_response = llama.generate([text], temperature=0.1, max_length=250)[0][0]#[len(text):]
+        #TODO: use stop words or maybe try to resample whenever it gives wierd results
+        raw_response = llama.generate([text], temperature=1., max_length=250)[0][0]#[len(text):]
 
         #raw_response = llama.generate([text], temperature=temp, top_p=1, max_length=10, stop_words=["\n", ";"])[0][0][len(text):]
         #response = raw_response.replace(' ', '').replace(',', '').rstrip('\n').rstrip(';')
@@ -84,7 +85,29 @@ if __name__ == "__main__":
         #instructions = 'A classification problem consists of a list of input-target pairs. Each input is a vector of length 2, each entry takes continuous values between 0 and 1. The target is a function of the input vector and takes values of either A or B. Please generate 10 new input-target pairs in the same format as the example. Here are the input-target pairs for an example classification problem: 1. Input: [0.5, 0.5], Target: A; 2. Input: [0.35, 0.31], Target: B; 3. Input: [0.12, 0.45], Target: B; 10. Input: [0.23, 0.46], Target: A; Your output: 1. Input:' # 3. Input: [0.34, 0.56], Target: B;'
         
         ## same as above but with sampled random numbers provided as input pairs
-        instructions = f'A classification problem consists of a list of input-target pairs. Each input is a vector of length 2, each entry takes continuous values between 0 and 1. The target is a function of the input vector and takes values of either A or B. Please generate 10 new input-target pairs in the same format as the example. Here are the input-target pairs for an example classification problem: 1. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; 2. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 3. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 10. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; Your output: 1. Input:' # 3. Input: [0.34, 0.56], Target: B;'
+        #instructions = f'A classification problem consists of a list of input-target pairs. Each input is a vector of length 2, each entry takes continuous values between 0 and 1. The target is a function of the input vector and takes values of either A or B. Please generate 10 new input-target pairs in the same format as the example. Here are the input-target pairs for an example classification problem: 1. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; 2. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 3. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 10. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; Your output: 1. Input:' # 3. Input: [0.34, 0.56], Target: B;'
+        
+        ## reordered the text a bit to see if I reduce the repeats in the sample (does it help?)
+        ## tried explicity adding do not include input-output pairs from the example did not help
+        #instructions = f'A classification problem consists of a list of input-target pairs. Each input is a vector of length 2, each entry takes continuous values between 0 and 1. The target is a function of the input vector and takes values of either A or B. Here are the input-target pairs for an example classification problem: 1. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; 2. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 3. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 10. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; Please generate 10 new input-target pairs in the same format as the example. Your output: 1. Input:' # 3. Input: [0.34, 0.56], Target: B;'
+
+        # reword a bit but still repeats the samples
+        #instructions = f'A classification problem consists of input-target pairs. Each input is a list of length 2, each entry takes continuous values between 0 and 1. The target is a function of the input list and takes values of either A or B. Here are some input-target pairs for an example classification problem: 1. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; 2. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 3. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: B; 10. Input: [{round(np.random.rand(1)[0],2)}, {round(np.random.rand(1)[0],2)}], Target: A; Please generate 10 input-target pairs for a new classification problem in the same format as the example. Your output: 1. Input:' # 3. Input: [0.34, 0.56], Target: B;'
+        
+        # random_samples = np.random.rand(8).round(2)
+        # instructions = f'A classification problem consists of a list of input-target pairs.\
+        #                     Each input is a list of length 2, each entry takes continuous values between 0 and 1.\
+        #                     The target is a function of the input vector and takes values of either A or B.\
+        #                     Here are some input-target pairs for an example classification problem: \
+        #                     1. Input: [{random_samples[0]}, {random_samples[1]}], Target: A;\
+        #                     2. Input: [{random_samples[2]}, {random_samples[3]}], Target: B;\
+        #                     3. Input: [{random_samples[4]}, {random_samples[5]}], Target: B;\
+        #                     10. Input: [{random_samples[6]}, {random_samples[7]}], Target: A;\
+        #                     Please generate 10 new input-target pairs in the same format as the example.\
+        #                     Your output:\
+        #                     1. Input:\
+        #                 '
+        
         
         # reformat above the way below doesn't seem yield the same results
         # instructions = """
@@ -101,27 +124,44 @@ if __name__ == "__main__":
         #                 1. Input: 
         #                 """
                         # 3. Input: [0.34, 0.56], Target: B;'
-        
+        `` 
+
         # This one interestingly generates code for generate data points
         # instructions = """ 
-        #                 A classification problem consists of a list of input-target pairs. 
-        #                 Each input is a vector of length 2, each entry takes continuous values between 0 and 1. 
-        #                 The target is a function of the input vector and takes values of either A or B. 
+        #                 A classification problem consists of a list of input-target pairs. \
+        #                 Each input, x, is a vector of length 2 x=[x1, x2], each entry takes continuous values between 0 and 1. \
+        #                 The target is a function of the input vector and takes values of either y=A or y=B. \
         #                 Here are the input-target pairs from an example classification problem delimited by triple backticks: 
         #                 ```
-        #                    1. Input: [0.5, 0.5], Target: A; 
+        #                    1. x= [
         #                    2. Input: [0.35, 0.31], Target: B; 
         #                    3. Input: [0.12, 0.45], Target: B; 
         #                    …
         #                    10. Input: [0.23, 0.46], Target: A;
-        #                 ``` 
+        #                 ```
         #                 Please generate 10 new input-target pairs in the same format as the example. 
         #                 Your output: 
-        #                 1. Input:
+        #                 x=[
         #                 """
+
+        ## playing with Julian
+        # instructions = f"A classification problem consists of a list of input-target pairs."\
+        #                 " Each input, x, is a vector of length 2 x=[x1, x2], each entry takes continuous values between 0 and 1."\
+        #                 " The target, y, is a function of the input vector and takes values of either y=A or y=B.\n\n"\
+        #                 "The following is 10 generated input-target pairs:\n"\
+        #                 "x=["
+
+        # rewording the above prompt -- this consistently gives new dataponts 
+        instructions = f"Each classification problem consists of a collection of input-target pairs."\
+                        " Each input, x, is a vector of length 2, x=[x1, x2], containing inputs features, with each feature taking continuous values between 0 and 1."\
+                        " The target, y, is a function of the input vector and takes values of either y=A or y=B.\n\n"\
+                        " The following is 10 generated input-target pairs for one such classification problem:\n"\
+                        "x=["
+        import ipdb; ipdb.set_trace()
 
         for t in range(num_trials):
             prompt = instructions 
+            # action = act(instructions, run_gpt)
             print('Prompt:')
             print(prompt)
             print("\n")
@@ -140,7 +180,6 @@ if __name__ == "__main__":
         #df = pd.DataFrame(data, columns=['run', 'example', 'dim0', 'dim1', 'target'])
         #print(df)
         #df.to_csv('/u/ajagadish/vanilla-llama/optimism-bias/data/run_' + str(run) + '.csv')
-        
         
         
 

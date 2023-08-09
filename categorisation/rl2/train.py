9080b49f2d685de7a65f7735be6ddc971edb2fa9
akjagadish <akshaykjagadish@gmail.com>
2023-08-09 09:57:06 +0200

    train meta_L model on label flipped and shuffled dataset

--- categorisation/rl2/train.py
+++ categorisation/rl2/train.py
@@ -62,11 +62,10 @@ def run_old(env_name, num_episodes, synthetic, max_steps, print_every, save_ever
         
     return losses, accuracy
 
-def run(env_name, num_episodes, synthetic, max_steps, print_every, save_every, num_hidden, save_dir, device, lr, batch_size=64):
+def run(env_name, num_episodes, synthetic, max_steps, noise, shuffle, print_every, save_every, num_hidden, save_dir, device, lr, batch_size=64):
 
     writer = SummaryWriter('runs/' + save_dir)
-    env = CategorisationTask(data=env_name, max_steps=max_steps, batch_size=batch_size, synthetic_data=synthetic, device=device).to(device)
-    #model = RL2(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden, num_layers=1).to(device)
+    env = CategorisationTask(data=env_name, max_steps=max_steps, batch_size=batch_size, synthetic_data=synthetic, noise=noise, shuffle_trials=shuffle, device=device).to(device)
     model = MetaLearner(num_input=env.num_dims, num_output=env.num_choices, num_hidden=num_hidden, num_layers=1).to(device)
     
     optimizer = optim.Adam(model.parameters(), lr=lr)
@@ -120,6 +119,8 @@ if __name__ == "__main__":
     parser.add_argument('--save-dir', default='trained_models/', help='directory to save models')
     parser.add_argument('--test', action='store_true', default=False, help='test runs')
     parser.add_argument('--synthetic', action='store_true', default=False, help='train models on synthetic data')
+    parser.add_argument('--noise', type=float, default=0., help='noise level')
+    parser.add_argument('--shuffle', action='store_true', default=False, help='shuffle trials')
 
     args = parser.parse_args()
     use_cuda = not args.no_cuda and torch.cuda.is_available()
@@ -130,7 +131,11 @@ if __name__ == "__main__":
         if args.test:
             save_dir = f'{args.save_dir}env={args.env_name}_num_episodes{str(args.num_episodes)}_num_hidden={str(args.num_hidden)}_lr{str(args.lr)}_run={str(args.first_run_id + i)}_test.pt'
         else:
-            save_dir = f'{args.save_dir}env={args.env_name}_num_episodes{str(args.num_episodes)}_num_hidden={str(args.num_hidden)}_lr{str(args.lr)}_run={str(args.first_run_id + i)}.pt'
+            save_dir = f'{args.save_dir}env={args.env_name}_num_episodes{str(args.num_episodes)}_num_hidden={str(args.num_hidden)}_lr{str(args.lr)}_noise{str(args.noise)}_shuffle{str(args.shuffle)}_run={str(args.first_run_id + i)}.pt'
         if args.synthetic:
             save_dir = save_dir.replace('.pt', '_synthetic.pt')
-        run(env_name, args.num_episodes, args.synthetic, args.max_steps, args.print_every, args.save_every, args.num_hidden, save_dir, device, args.lr)
+        # if args.noise > 0.:
+        #     save_dir = save_dir.replace('.pt', f'_noise{str(args.noise)}.pt')
+        # if args.shuffle:
+        #     save_dir = save_dir.replace('.pt', f'_shuffle{str(args.shuffle)}.pt')
+        run(env_name, args.num_episodes, args.synthetic, args.max_steps, args.noise, args.shuffle, args.print_every, args.save_every, args.num_hidden, save_dir, device, args.lr)

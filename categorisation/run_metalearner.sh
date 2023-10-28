fbe0d89a69282e5da777e9981f41377c56f6f88f
akjagadish <akshaykjagadish@gmail.com>
2023-10-28 13:49:00 +0200

    train transformer model on 10k claude generated tasks

--- categorisation/run_metalearner.sh
+++ categorisation/run_metalearner.sh
@@ -20,22 +20,14 @@ module load pytorch_distributed/gpu-cuda-11.6/1.13.0
 # pip3 install --user fire sentencepiece ipdb accelerate tqdm
 export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
-# python rl2/train.py --num-episodes 100000 --save-every 1000 --print-every 100 --test --synthetic --max-steps 96
-# python rl2/train.py --num-episodes 500000 --save-every 1000 --print-every 100 --max-steps 70 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000 --first-run-id 0 --noise 0.0
-# python rl2/train.py --num-episodes 1000000 --save-every 100 --print-every 100 
 
-## uniform data
+## synthetic data
 # python rl2/train.py --num-episodes 500000 --save-every 1000 --print-every 100 --max-steps 70 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000 --first-run-id 0 --noise 0.0 --synthetic
 
-# prompt version 1
-# python rl2/train.py --num-episodes 500000 --save-every 1000 --print-every 100 --max-steps 70 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000_pversion1 --first-run-id 0 --noise 0.0 
-# python rl2/train.py --num-episodes 500000 --save-every 1000 --print-every 100 --max-steps 70 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000_pversion1 --first-run-id 0 --noise 0.0 --shuffle
+## llm generated data: prompt version 1
+# python rl2/train_transformer.py --num-episodes 500000 --save-every 100 --print-every 100 --max-steps 250 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000_pversion1  --first-run-id 0 --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64 --shuffle
+# python rl2/train_transformer.py --num-episodes 500000 --save-every 100 --print-every 100 --max-steps 250 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000_pversion1  --first-run-id 0 --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64 
 
-# python rl2/train.py --num-episodes 100 --save-every 10 --print-every 100 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000 --first-run-id 0 --noise 0.0 --synthetic
-# python rl2/train_transformer.py --num-episodes 10000 --save-every 10 --print-every 100 --max-steps 70 --env-name tranformer_metalearning_synthetic --first-run-id 0 --noise 0.0 --synthetic --model-name transformer
-# python rl2/train_transformer.py --num-episodes 500000 --save-every 10 --print-every 100 --max-steps 70 --env-name tranformer_metalearning_synthetic --first-run-id 0 --noise 0.0 --synthetic --model-name transformer --num_hidden 128 --num_layers 1 --d_model 256 --num_head 2
-
-# python rl2/train_transformer.py --num-episodes 500000 --save-every 1000 --print-every 100 --max-steps 200 --env-name transformer_metalearning_synthetic  --first-run-id 0 --noise 0.0 --model-name transformer --synthetic --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8
-# python rl2/train_transformer.py --num-episodes 500000 --save-every 100 --print-every 100 --max-steps 200 --env-name transformer_metalearning_synthetic  --first-run-id 0 --noise 0.0 --model-name transformer --synthetic --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8
-
-python rl2/train_transformer.py --num-episodes 500000 --save-every 100 --print-every 100 --max-steps 250 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks14000_pversion1  --first-run-id 0 --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64 --shuffle
+## llm generated data: prompt version 4
+# python rl2/train_transformer.py --num-episodes 500000 --save-every 100 --print-every 100 --max-steps 250 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4  --first-run-id 0 --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64 --shuffle
+python rl2/train_transformer.py --num-episodes 500000 --save-every 100 --print-every 100 --max-steps 250 --env-name claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4  --first-run-id 0 --noise 0.0 --model-name transformer --num_hidden 256 --num_layers 6 --d_model 64 --num_head 8 --batch_size 64

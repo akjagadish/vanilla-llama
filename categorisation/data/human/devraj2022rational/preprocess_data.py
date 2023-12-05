0723a5135339f3edfb9c8360096c026ab5a6dd3e
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2023-12-05 13:24:32 +0100

    preprocess devraj2022 data to fit prototype model

--- categorisation/data/human/devraj2022rational/preprocess_data.py
+++ categorisation/data/human/devraj2022rational/preprocess_data.py
@@ -26,6 +26,7 @@ stimulus_features = {0: [0, 0, 0, 0, 0, 0], 1: [1, 0, 0, 0, 0, 0], 2: [0, 1, 0, 
                      4: [0, 0, 0, 0, 1, 0], 5: [0, 0, 0, 0, 0, 1], 6: [1, 1, 1, 1, 0, 1], 7: [1, 1, 1, 1, 1, 1], \
                      8: [0, 1, 1, 1, 1, 1], 9: [1, 0, 1, 1, 1, 1], 10: [1, 1, 0, 1, 1, 1], 11: [1, 1, 1, 0, 1, 1],\
                     12: [1, 1, 1, 1, 1, 0], 13: [0, 0, 0, 0, 1, 0]}
+prototype_features = {1: [0, 0, 0, 0, 0, 0], 2: [1, 1, 1, 1, 1, 1]}
                      
 # make a new dataframe with each row containing features of the task of one trial for a given participant
 new_df = pd.DataFrame(columns=['participant', 'task', 'trial', 'choice', 'correct_choice', 'block', 'trial_segment', 'condition', 'category', 'all_features', 'feature1', 'feature2', 'feature3', 'feature4', 'feature5', 'feature6'])
@@ -34,14 +35,18 @@ for participant_id in range(len(stimulus_sequence)):
         block_id = trial_id//14
         trial_segment = trial_id//56
         new_df = new_df.append(pd.DataFrame({'participant': [participant_id], 'task': [0], 'block': [block_id], 'trial_segment': [trial_segment], 'condition': [conditions[participant_id]], \
-                                             'trial': [trial_id], 'category': [true_categories[participant_id][trial_id]], 'choice': [choices[participant_id][trial_id]], \
-                                             'correct_choice': [true_categories[participant_id][trial_id]], 'all_features': [str(stimulus_features[stimulus_sequence[participant_id][trial_id]])], \
+                                             'trial': [trial_id], 'category': [true_categories[participant_id][trial_id]], 'choice': [choices[participant_id][trial_id]-1], \
+                                             'correct_choice': [true_categories[participant_id][trial_id]-1], 'all_features': [str(stimulus_features[stimulus_sequence[participant_id][trial_id]])], \
                                              'feature1': [stimulus_features[stimulus_sequence[participant_id][trial_id]][0]], 'feature2': [stimulus_features[stimulus_sequence[participant_id][trial_id]][1]], \
                                              'feature3': [stimulus_features[stimulus_sequence[participant_id][trial_id]][2]], 'feature4': [stimulus_features[stimulus_sequence[participant_id][trial_id]][3]], \
-                                             'feature5': [stimulus_features[stimulus_sequence[participant_id][trial_id]][4]], 'feature6': [stimulus_features[stimulus_sequence[participant_id][trial_id]][5]]}), ignore_index=True)
+                                             'feature5': [stimulus_features[stimulus_sequence[participant_id][trial_id]][4]], 'feature6': [stimulus_features[stimulus_sequence[participant_id][trial_id]][5]], \
+                                             'prototype_feature1': [prototype_features[true_categories[participant_id][trial_id]][0]], 'prototype_feature2': [prototype_features[true_categories[participant_id][trial_id]][1]], \
+                                             'prototype_feature3': [prototype_features[true_categories[participant_id][trial_id]][2]], 'prototype_feature4': [prototype_features[true_categories[participant_id][trial_id]][3]], \
+                                             'prototype_feature5': [prototype_features[true_categories[participant_id][trial_id]][4]], 'prototype_feature6': [prototype_features[true_categories[participant_id][trial_id]][5]]}), ignore_index=True)
+                                             
 
 # save the dataframe as a csv file
-new_df.to_csv('./devraj2022rational.csv', index=False)
+new_df.to_csv('../devraj2022rational.csv', index=False)
 
 # plot accuracy of choice for each participant over trial segments using new dataframe
 accuracy = []

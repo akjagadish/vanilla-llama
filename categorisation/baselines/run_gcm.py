739658983a55d22ca23d2425e8f5209d35d6436d
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2023-12-08 19:15:10 +0100

    run gcm models for metalearner simulations for different betas values

--- categorisation/baselines/run_gcm.py
+++ categorisation/baselines/run_gcm.py
@@ -2,6 +2,7 @@ import pandas as pd
 from gcm import GeneralizedContextModel
 import sys
 import numpy as np
+import argparse
 sys.path.append('../')
 
 ## badham et al.
@@ -87,28 +88,42 @@ sys.path.append('../')
 #          , r2s=r2s, lls=lls, params=np.stack(params_list), opt_method=opt_method)
 
 
-beta=0.1
-df = pd.read_csv(f'../data/meta_learner/smith_categorisation__tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_beta={beta}_num_trials=616_num_runs=1.csv')
-##### REMOVE THIS LINE AFTER TESTING #####
-# df = df[df['participant']==0] # keep only first participant for testing
-##### REMOVE ABOVE LINE AFTER TESTING #####
-num_runs, num_blocks, num_iter = 1, 11, 1
-loss = 'mse_transfer'
-opt_method = 'minimize'
-NUM_TASKS, NUM_FEATURES = 1, 6
-lls, r2s, params_list = [], [], []
-for idx in range(num_runs):
-    gcm = GeneralizedContextModel(num_features=NUM_FEATURES, distance_measure=1, num_iterations=num_iter, opt_method=opt_method, loss=loss)
-    ll, r2, params = gcm.fit_metalearner(df, num_blocks=num_blocks, reduce='sum')
-    params_list.append(params)
-    lls.append(ll)
-    r2s.append(r2)
-    print(lls[idx], r2s[idx])
-    print(f'mean mse across blocks: {lls[idx].mean()} \n')
-    print(f'mean pseudo-r2 across blocks: {r2s[idx].mean()}')
 
-# save the r2 and ll values
-lls = np.array(lls)
-r2s = np.array(r2s)
-np.savez(f'../data/meta_learner/gcm_metalearner_devrajstask_beta={beta}_runs={num_runs}_iters={num_iter}_blocks={num_blocks}_tasks={NUM_TASKS}'\
-         , r2s=r2s, lls=lls, params=np.stack(params_list), opt_method=opt_method)
+def fit_gcm_to_metalearner(beta, num_runs=1, num_blocks=11, num_iter=1, num_tasks=1, num_features=6, opt_method='minimize', loss='mse_transfer'):
+    # beta=0.1
+    df = pd.read_csv(f'../data/meta_learner/smith_categorisation__tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_beta={beta}_num_trials=616_num_runs=10.csv')
+    # num_runs, num_blocks, num_iter = 1, 11, 1
+    # loss = 'mse_transfer'
+    # opt_method = 'minimize'
+    NUM_TASKS, NUM_FEATURES = 1, 6
+    lls, r2s, params_list = [], [], []
+    for idx in range(num_runs):
+        gcm = GeneralizedContextModel(num_features=NUM_FEATURES, distance_measure=1, num_iterations=num_iter, opt_method=opt_method, loss=loss)
+        ll, r2, params = gcm.fit_metalearner(df, num_blocks=num_blocks, reduce='sum')
+        params_list.append(params)
+        lls.append(ll)
+        r2s.append(r2)
+        print(lls[idx], r2s[idx])
+        print(f'mean mse across blocks: {lls[idx].mean()} \n')
+        print(f'mean pseudo-r2 across blocks: {r2s[idx].mean()}')
+
+    # save the r2 and ll values
+    lls = np.array(lls)
+    r2s = np.array(r2s)
+    np.savez(f'../data/meta_learner/gcm_metalearner_devrajstask_beta={beta}_runs={num_runs}_iters={num_iter}_blocks={num_blocks}_tasks={NUM_TASKS}'\
+            , r2s=r2s, lls=lls, params=np.stack(params_list), opt_method=opt_method)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='fit gcm to meta-learner choices')
+    parser.add_argument('--beta', type=float, required=True, help='beta value')
+    parser.add_argument('--num-iter', type=int, required=True, default=1, help='number of iterations')
+    parser.add_argument('--num-runs', type=int, required=False,  default=11, help='number of runs')
+    parser.add_argument('--num-blocks', type=int,required=False, default=11, help='number of blocks')
+    parser.add_argument('--num-tasks', type=int, required=False, default=1, help='number of tasks')
+    parser.add_argument('--num-features', type=int, required=False, default=6, help='number of features')
+    parser.add_argument('--opt-method', type=str, required=False, default='minimize', help='optimization method')
+    parser.add_argument('--loss', type=str, required=False, default='mse_transfer', help='loss function')
+    args = parser.parse_args()
+
+    fit_gcm_to_metalearner(beta=args.beta, num_runs=args.num_runs, num_blocks=args.num_blocks, num_iter=args.num_iter, num_tasks=args.num_tasks, num_features=args.num_features, opt_method=args.opt_method, loss=args.loss)

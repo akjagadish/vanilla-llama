8be361e63c741602655005feac993122a3538f96
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2023-12-29 14:21:25 +0100

    simulate data usign rmc model

--- categorisation/rl2/simulate_data.py
+++ categorisation/rl2/simulate_data.py
@@ -1,10 +1,15 @@
-from envs import SyntheticCategorisationTask
+from envs import SyntheticCategorisationTask, RMCTask
 import argparse
 import torch
 
-def simulate(num_tasks, nonlinear, num_dims, max_steps, noise, shuffle, device, batch_size=64):
+def simulate(num_tasks, rmc, nonlinear, num_dims, max_steps, noise, shuffle, device, batch_size=64):
 
-    env = SyntheticCategorisationTask(nonlinear=nonlinear, num_dims=num_dims, max_steps=max_steps, batch_size=batch_size, noise=noise, shuffle_trials=shuffle, device=device).to(device)
+    if rmc:
+        assert num_dims == 3, 'RMC only supports 3 dimensions'
+        env = RMCTask(num_dims=num_dims, max_steps=max_steps, batch_size=batch_size, noise=noise, shuffle_trials=shuffle, device=device).to(device)
+    else:
+        env = SyntheticCategorisationTask(nonlinear=nonlinear, num_dims=num_dims, max_steps=max_steps, batch_size=batch_size, noise=noise, shuffle_trials=shuffle, device=device).to(device)
+    
     env.save_synthetic_data(num_tasks)
 
 
@@ -17,8 +22,10 @@ if __name__ == "__main__":
     parser.add_argument('--noise', type=float, default=0.0, help='noise level')
     parser.add_argument('--shuffle', type=bool, default=False, help='shuffle trials')
     parser.add_argument('--nonlinear', action='store_true', default=False, help='simulate nonlinear synthetic data')
+    parser.add_argument('--rmc', action='store_true', default=False, help='simulate rmc data')
+
     args = parser.parse_args()
     use_cuda = torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-    simulate(args.num_tasks, args.nonlinear, args.num_dims, args.max_steps, args.noise, args.shuffle, device, args.batch_size)
+    simulate(args.num_tasks, args.rmc, args.nonlinear, args.num_dims, args.max_steps, args.noise, args.shuffle, device, args.batch_size)
 

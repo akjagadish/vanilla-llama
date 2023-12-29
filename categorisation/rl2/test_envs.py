9ce979f3753605ed3240248c543702691edd7f58
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2023-12-29 13:37:14 +0100

    test rmc task

--- categorisation/rl2/test_envs.py
+++ categorisation/rl2/test_envs.py
@@ -3,8 +3,11 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.utils.tensorboard import SummaryWriter
-from envs import SmithsTask
+from envs import SmithsTask, RMCTask
 
 
-task = SmithsTask(rule='linear')
+# task = SmithsTask(rule='linear')
+# task.sample_batch()
+
+task = RMCTask(batch_size=64, max_steps=100)
 task.sample_batch()

d372c04d19deb4a261429b21ad2721360586bd59
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-01-09 00:52:32 +0100

    add plots for ICML24 submission

--- categorisation/plots.py
+++ categorisation/plots.py
@@ -1140,7 +1140,7 @@ def model_comparison(list_models=None, task_name = 'smithstask'):
     
     models = ['badham2017_env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_beta_sweep.npy',\
               'badham2017_env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear_beta_sweep', \
-            'badham2017_env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic_beta_sweep.npy']
+              'badham2017_env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic_beta_sweep.npy']
     models = list_models if list_models is not None else models
     
     nlls,fitted_betas = [], []
@@ -1185,4 +1185,319 @@ def model_comparison(list_models=None, task_name = 'smithstask'):
         ax[m_idx].set_title(f'{task_titles[task_name]}', fontsize=FONTSIZE)
         sns.despine()
 
-    
+def model_comparison_badham2017(FIGSIZE=(6,5)):
+    models = ['badham2017_env=claude_generated_tasks_paramsNA_dim3_data100_tasks11518_pversion4_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_soft_sigmoid_differential_evolution',\
+              'badham2017_env=rmc_tasks_dim3_data100_tasks11499_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_rmc_soft_sigmoid_differential_evolution',
+              'badham2017_env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_syntheticnonlinear_soft_sigmoid_differential_evolution',\
+              'badham2017_env=dim3synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic_soft_sigmoid_differential_evolution',\
+              'badham2017_gcm_runs=1_iters=1_blocks=1_loss=nll',\
+              'badham2017_rulex_runs=1_iters=1_blocks=1_loss=nll_exception=True',\
+              'badham2017_rulex_runs=1_iters=1_blocks=1_loss=nll_exception=False',\
+              'badham2017_pm_runs=1_iters=1_blocks=1_loss=nll',\
+                ]
+    nlls,fitted_betas, r2s = [], [], []
+    model_accs = []
+    bics = []
+    NUM_TASKS = 4
+    NUM_TRIALs = 96
+    num_trials = NUM_TRIALs*NUM_TASKS
+    # FONTSIZE = 16
+    # MODELS = ['ERMI', 'RMC-MI', 'L-MI', 'PFN-MI', 'GCM', 'Rulex', 'Rule',  'PM']
+    MODELS = ['ERMI', 'RMC-\nMI', 'L-MI', 'PFN-\nMI', 'GCM', 'Rulex', 'Rule',  'PM']
+
+
+    for model_name in models:
+        fits =  np.load(f'{SYS_PATH}/categorisation/data/model_comparison/{model_name}.npz')
+        if 'model=transformer' in model_name:
+            betas, pnlls, pr2s = fits['betas'], fits['nlls'], fits['pr2s']
+            model_accs.append(fits['accs'].max(0).mean())
+            nlls_min_nlls = np.array(pnlls).squeeze()
+            pr2s_min_nll = np.array(pr2s).squeeze()
+            fitted_betas.append(betas)
+            num_parameters = 1 
+            bic = np.array(nlls_min_nlls)*2 + np.log(num_trials)*num_parameters
+        elif ('gcm' in model_name) or ('pm' in model_name):
+            betas, pnlls, pr2s = fits['params'], fits['lls'], fits['r2s']
+            # summing the fits for the four conditions separately; hence the total number of parameters is model_parameters*NUM_TASKS
+            nlls_min_nlls = np.array(pnlls).squeeze().sum(1)
+            pr2s_min_nll = np.array(pr2s).squeeze().mean(1)
+            num_parameters = 5*NUM_TASKS if ('gcm' in model_name) else 11*NUM_TASKS
+            bic = np.array(nlls_min_nlls)*2 + np.log(num_trials)*num_parameters
+            fitted_betas.append(betas.squeeze()[..., 1].mean(1))
+        elif ('rulex' in model_name):
+            betas, pnlls, pr2s = fits['params'], fits['lls'], fits['r2s']
+            nlls_min_nlls = np.array(pnlls).squeeze().sum(1)
+            pr2s_min_nll = np.array(pr2s).squeeze().mean(1)
+            num_parameters = 2*NUM_TASKS
+            bic = np.array(nlls_min_nlls)*2 + np.log(num_trials)*num_parameters
+
+        nlls.append(nlls_min_nlls)
+        r2s.append(pr2s_min_nll)
+        bics.append(bic)
+
+    # keep models and choose colors
+    num_participants = len(nlls[0])
+    MODELS = MODELS[:len(nlls)]
+    # set colors depending on number of models in MODELS
+    colors = ['#173b4f', '#8b9da7', '#5d7684', '#2f4a5a', '#0d2c3d', '#4d6a75', '#748995', '#a2c0a9'][:len(nlls)]
+
+
+    # compare mean nlls across models in a bar plot
+    f, ax = plt.subplots(1, 1, figsize=FIGSIZE)
+    bar_positions = np.arange(len(nlls))*0.5
+    ax.bar(bar_positions, np.array(nlls).mean(1), color=colors, width=0.4)
+    ax.errorbar(bar_positions, np.array(nlls).mean(1), yerr=np.array(nlls).std(1)/np.sqrt(num_participants-1), c='k', lw=3, fmt="o")
+    # add chance level line for 616 trials with binary choices
+    ax.axhline(y=-np.log(0.5)*num_trials, color='k', linestyle='--', lw=3)
+    ax.set_xlabel('Models', fontsize=FONTSIZE)
+    ax.set_ylabel('NLL', fontsize=FONTSIZE)
+    ax.set_xticks(bar_positions)  # Set x-tick positions to bar_positions
+    ax.set_xticklabels(MODELS, fontsize=FONTSIZE-5)  # Assign category names to x-tick labels
+    # ax.set_title(f'Model comparison for Badham et al. (2017)', fontsize=FONTSIZE)
+    # plt.xticks(fontsize=FONTSIZE-2)
+    plt.yticks(fontsize=FONTSIZE-2)
+    sns.despine()
+    f.tight_layout()
+    plt.show()
+
+    # compare mean BICS across models in a bar plot
+    f, ax = plt.subplots(1, 1, figsize=FIGSIZE)
+    bar_positions = np.arange(len(bics))*1.5
+    ax.bar(bar_positions, np.array(bics).mean(1), color=colors, width=1.)
+    ax.errorbar(bar_positions, np.array(bics).mean(1), yerr=np.array(bics).std(1)/np.sqrt(num_participants-1), c='k', lw=3, fmt="o")
+    # add chance level line for 616 trials with binary choices
+    # ax.axhline(y=-np.log(0.5)*num_trials*2, color='k', linestyle='--', lw=3)
+    ax.set_xlabel('Models', fontsize=FONTSIZE)
+    ax.set_ylabel('BIC', fontsize=FONTSIZE)
+    ax.set_xticks(bar_positions)  # Set x-tick positions to bar_positions
+    ax.set_xticklabels(MODELS, fontsize=FONTSIZE-6)  # Assign category names to x-tick labels
+    # ax.set_title(f'Model comparison for Badham et al. (2017)', fontsize=FONTSIZE)
+    # plt.xticks(fontsize=FONTSIZE-2)
+    plt.yticks(fontsize=FONTSIZE-2)
+    sns.despine()
+    f.tight_layout()
+    plt.show()
+    f.savefig(f'{SYS_PATH}/categorisation/figures/bic_badham2017.svg', bbox_inches='tight', dpi=300)
+
+    # compare mean r2s across models in a bar plot
+    f, ax = plt.subplots(1, 1, figsize=FIGSIZE)
+    bar_positions = np.arange(len(r2s))*0.5
+    ax.bar(bar_positions, np.array(r2s).mean(1), color=colors, width=0.4)
+    ax.errorbar(bar_positions, np.array(r2s).mean(1), yerr=np.array(r2s).std(1)/np.sqrt(num_participants-1), c='k', lw=3, fmt="o")
+    ax.set_xlabel('Models', fontsize=FONTSIZE)
+    ax.set_ylabel('R2', fontsize=FONTSIZE)
+    ax.set_xticks(bar_positions)  # Set x-tick positions to bar_positions
+    ax.set_xticklabels(MODELS, fontsize=FONTSIZE-5)  # Assign category names to x-tick labels
+
+    # ax.set_title(f'Model comparison for  Badham et al. (2017)', fontsize=FONTSIZE)
+    plt.yticks(fontsize=FONTSIZE-2)
+    sns.despine()
+    f.tight_layout()
+    plt.show()   
+
+def model_comparison_devraj2022(FIGSIZE=(6,5)):
+    models = ['devraj2022_env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_soft_sigmoid_differential_evolution', \
+              'devraj2022_env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=2_synthetic_soft_sigmoid_differential_evolution', \
+              'devraj2022_env=dim6synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=2_syntheticnonlinear_soft_sigmoid_differential_evolution',\
+              'devraj2022_gcm_runs=1_iters=1_blocks=1_loss=nll',\
+              'devraj2022_pm_runs=1_iters=1_blocks=1_loss=nll',\
+              'devraj2022_rulex_runs=1_iters=1_blocks=1_loss=nll_exception=True', \
+              'devraj2022_rulex_runs=1_iters=1_blocks=1_loss=nll_exception=False', 
+         ]
+    nlls,fitted_betas, r2s = [], [], []
+    model_accs = []
+    bics = []
+    NUM_TASKS = 1
+    NUM_TRIALs = 616
+    num_trials = NUM_TRIALs*NUM_TASKS
+    # FONTSIZE = 16
+    MODELS = ['ERMI', 'L-MI', 'PFN-\nMI', 'GCM', 'PM', 'Rulex', 'Rule']
+
+    for model_name in models:
+        fits =  np.load(f'{SYS_PATH}/categorisation/data/model_comparison/{model_name}.npz')
+        if 'model=transformer' in model_name:
+            betas, pnlls, pr2s = fits['betas'], fits['nlls'], fits['pr2s']
+            model_accs.append(fits['accs'].max(0).mean())
+            nlls_min_nlls = np.array(pnlls).squeeze()
+            pr2s_min_nll = np.array(pr2s).squeeze()
+            fitted_betas.append(betas)
+            num_parameters = 1 
+            bic = np.array(nlls_min_nlls)*2 + np.log(num_trials)*num_parameters
+        elif ('gcm' in model_name) or ('pm' in model_name):
+            betas, pnlls, pr2s = fits['params'], fits['lls'], fits['r2s']
+            # summing the fits for the four conditions separately; hence the total number of parameters is model_parameters*NUM_TASKS
+            nlls_min_nlls = np.array(pnlls).squeeze()
+            pr2s_min_nll = np.array(pr2s).squeeze()
+            num_parameters = 8*NUM_TASKS
+            bic = np.array(nlls_min_nlls)*2 + np.log(num_trials)*num_parameters
+            fitted_betas.append(betas.squeeze()[:, 1])
+        elif ('rulex' in model_name):
+            betas, pnlls, pr2s = fits['params'], fits['lls'], fits['r2s']
+            nlls_min_nlls = np.array(pnlls).squeeze()
+            pr2s_min_nll = np.array(pr2s).squeeze()
+            num_parameters = 2*NUM_TASKS
+            bic = np.array(nlls_min_nlls)*2 + np.log(num_trials)*num_parameters
+
+        nlls.append(nlls_min_nlls)
+        r2s.append(pr2s_min_nll)
+        bics.append(bic)
+
+    # keep models and choose colors
+    num_participants = len(nlls[0])
+    MODELS = MODELS[:len(nlls)]
+    # set colors depending on number of models in MODELS
+    colors = ['#173b4f', '#8b9da7', '#5d7684', '#2f4a5a', '#0d2c3d', '#4d6a75', '#748995', '#a2c0a9'][:len(nlls)]
+
+
+    # compare mean nlls across models in a bar plot
+    f, ax = plt.subplots(1, 1, figsize=FIGSIZE)
+    bar_positions = np.arange(len(nlls))*0.5
+    ax.bar(bar_positions, np.array(nlls).mean(1), color=colors, width=0.4)
+    ax.errorbar(bar_positions, np.array(nlls).mean(1), yerr=np.array(nlls).std(1)/np.sqrt(num_participants-1), c='k', lw=3, fmt="o")
+    # add chance level line for 616 trials with binary choices
+    ax.axhline(y=-np.log(0.5)*num_trials, color='k', linestyle='--', lw=3)
+    ax.set_xlabel('Models', fontsize=FONTSIZE)
+    ax.set_ylabel('NLL', fontsize=FONTSIZE)
+    ax.set_xticks(bar_positions)  # Set x-tick positions to bar_positions
+    ax.set_xticklabels(MODELS, fontsize=FONTSIZE-5)  # Assign category names to x-tick labels
+    # ax.set_title(f'Model comparison for Badham et al. (2017)', fontsize=FONTSIZE)
+    # plt.xticks(fontsize=FONTSIZE-2)
+    plt.yticks(fontsize=FONTSIZE-2)
+    sns.despine()
+    f.tight_layout()
+    plt.show()
+
+    # compare mean BICS across models in a bar plot
+    f, ax = plt.subplots(1, 1, figsize=FIGSIZE)
+    bar_positions = np.arange(len(bics))*1.5
+    ax.bar(bar_positions, np.array(bics).mean(1), color=colors, width=1.)
+    ax.errorbar(bar_positions, np.array(bics).mean(1), yerr=np.array(bics).std(1)/np.sqrt(num_participants-1), c='k', lw=3, fmt="o")
+    # add chance level line for 616 trials with binary choices
+    # ax.axhline(y=-np.log(0.5)*num_trials*2, color='k', linestyle='--', lw=3)
+    ax.set_xlabel('Models', fontsize=FONTSIZE)
+    ax.set_ylabel('BIC', fontsize=FONTSIZE)
+    ax.set_xticks(bar_positions)  # Set x-tick positions to bar_positions
+    ax.set_xticklabels(MODELS, fontsize=FONTSIZE-6)  # Assign category names to x-tick labels
+    # ax.set_title(f'Model comparison for Badham et al. (2017)', fontsize=FONTSIZE)
+    # plt.xticks(fontsize=FONTSIZE-2)
+    plt.yticks(fontsize=FONTSIZE-2)
+    sns.despine()
+    f.tight_layout()
+    plt.show()
+    f.savefig(f'{SYS_PATH}/categorisation/figures/bic_devraj2022.svg', bbox_inches='tight', dpi=300)
+
+    # compare mean r2s across models in a bar plot
+    f, ax = plt.subplots(1, 1, figsize=FIGSIZE)
+    bar_positions = np.arange(len(r2s))*0.5
+    ax.bar(bar_positions, np.array(r2s).mean(1), color=colors, width=0.4)
+    ax.errorbar(bar_positions, np.array(r2s).mean(1), yerr=np.array(r2s).std(1)/np.sqrt(num_participants-1), c='k', lw=3, fmt="o")
+    ax.set_xlabel('Models', fontsize=FONTSIZE)
+    ax.set_ylabel('R2', fontsize=FONTSIZE)
+    ax.set_xticks(bar_positions)  # Set x-tick positions to bar_positions
+    ax.set_xticklabels(MODELS, fontsize=FONTSIZE-5)  # Assign category names to x-tick labels
+
+    # ax.set_title(f'Model comparison for  Badham et al. (2017)', fontsize=FONTSIZE)
+    plt.yticks(fontsize=FONTSIZE-2)
+    sns.despine()
+    f.tight_layout()
+    plt.show() 
+
+def gcm_pm_fitted_simulations():
+
+    models = ['ermi', 'synthetic', 'syntheticnonlinear']
+    f, ax = plt.subplots(1, len(models), figsize=(5*len(models),5))
+    colors = ['#173b4f', '#37761e']
+    for idx, model in enumerate(models):
+        fits_gcm = np.load(f'{SYS_PATH}/categorisation/data/fitted_simulation/devraj2022_gcm_runs=1_iters=10_blocks=11_loss=mse_transfer_model={model}.npz')
+        fits_pm = np.load(f'{SYS_PATH}/categorisation/data/fitted_simulation/devraj2022_pm_runs=1_iters=10_blocks=11_loss=mse_transfer_model={model}.npz')
+
+        # load mses
+        mses_gcm = fits_gcm['lls']
+        mses_pm = fits_pm['lls']
+        # mean mses across participants: mses are of shape (n_runs=1, n_participants, n_conditions=1, n_blocks)
+        mses_gcm = np.squeeze(mses_gcm)
+        mses_pm = np.squeeze(mses_pm)
+        # std error of mean across participants
+        stds_gcm = np.std(mses_gcm, axis=0)/np.sqrt(len(mses_gcm)-1)
+        stds_pm = np.std(mses_pm, axis=0)/np.sqrt(len(mses_pm)-1)
+
+        # plot mean mses across participants for each trial segment for both models
+        sns.lineplot(x=np.arange(11)+1, y=np.mean(mses_pm, axis=0), ax=ax[idx], color=colors[0], label='PM')
+        sns.lineplot(x=np.arange(11)+1, y=np.mean(mses_gcm, axis=0), ax=ax[idx], color=colors[1], label='GCM')
+        # add standard error of mean as error bars
+        ax[idx].fill_between(np.arange(11)+1, np.mean(mses_pm, axis=0)-stds_pm, np.mean(mses_pm, axis=0)+stds_pm, alpha=0.2, color=colors[0])
+        ax[idx].fill_between(np.arange(11)+1, np.mean(mses_gcm, axis=0)-stds_gcm, np.mean(mses_gcm, axis=0)+stds_gcm, alpha=0.2, color=colors[1])
+        ax[idx].set_xlabel('Trial segment', fontsize=FONTSIZE)
+        ax[idx].set_ylim([0, 1.])
+        ax[idx].set_xticks(np.arange(11)+1)
+        # set y ticks font size
+        ax[idx].tick_params(axis='y', labelsize=FONTSIZE-2)
+        ax[idx].set_xticklabels(np.arange(11)+1,fontsize=FONTSIZE-2)
+        if idx==0:
+            ax[idx].set_ylabel('Error', fontsize=FONTSIZE)
+            # remove bounding box around the legend
+            ax[idx].legend(frameon=False, fontsize=FONTSIZE-2)
+        else:
+            # remove legend
+            ax[idx].legend([], frameon=False, fontsize=FONTSIZE-2)
+        
+    sns.despine()
+    f.tight_layout()
+    plt.show()
+    f.savefig(f'{SYS_PATH}/categorisation/figures/gcm_pm_fitted_simulations.svg', bbox_inches='tight', dpi=300)
+
+def simulate_shepard1961(models=None, tasks=np.arange(1,7), betas=None, num_runs=5, num_trials=96, num_blocks=1, batch_size=64):
+
+    corrects = np.ones((len(models), len(tasks), batch_size, num_trials))
+    assert len(models)==len(betas), "Number of models and betas should be the same"
+    for m_idx, (model_name, beta) in enumerate(zip(models, betas)):
+        if model_name != 'humans':
+            for t_idx, task in enumerate(tasks):
+                model_path = f"/u/ajagadish/vanilla-llama/categorisation/trained_models/{model_name}.pt"
+                corrects[m_idx, t_idx] = evaluate_metalearner(task, model_path, 'shepard_categorisation', beta=beta, shuffle_trials=True, num_trials=num_trials, num_runs=num_runs)
+            
+    # models = ['human', 1, 2]
+    # corrects = np.random.rand(3, 6, 64, 256)
+    # model_name = 'test'
+
+    # compuate error rates across trials using corrects
+    errors = 1. - corrects.mean(2)
+
+
+    # compare the error rate over trials between different tasks meaned over noise levels, shuffles and shuffle_evals
+    f, axes = plt.subplots(1, len(models), figsize=(6*len(models), 5))
+    colors = ['#E0E1DD', '#B6B9B9', '#8C9295', '#616A72','#37434E','#0D1B2A']
+    # markers for the six types of rules in the plot: circle, cross, plus, inverted triangle, asterisk, triangle
+    markers = ['o', 'x', '+', '*', 'v', '^']
+
+    for idx, ax in enumerate(axes):
+
+        if models[idx]=='humans':
+            # load json file containing the data
+            with open('/u/ajagadish/vanilla-llama/categorisation/data/human/nosofsky1994.json') as json_file:
+                data = json.load(json_file)
+            for i, rule in enumerate(data.keys()):
+                ax.plot(np.arange(len(data[rule]['y']))+1, data[rule]['y'], label=f'Type {i+1}', lw=3, color=colors[i], marker=markers[i], markersize=8)
+        else:
+            for t_idx, task in enumerate(tasks):
+                block_errors = np.stack(np.split(errors[idx, t_idx], num_blocks)).mean(1)
+                ax.plot(np.arange(1, num_blocks+1), block_errors, label=f'Type {task}', lw=3, color=colors[t_idx], marker=markers[t_idx], markersize=8)
+
+        ax.set_xticks(np.arange(1, num_blocks+1))
+        ax.set_xlabel('Block', fontsize=FONTSIZE)
+        if idx==0:
+            ax.set_ylabel('Error rate', fontsize=FONTSIZE)
+        ax.set_ylim([-0.05, .55])
+        locs, labels = ax.get_xticks(), ax.get_xticklabels()
+        # Set new x-tick locations and labels
+        ax.set_xticks(locs[::2])
+        ax.set_xticklabels(np.arange(1, num_blocks+1)[::2]+1, fontsize=FONTSIZE-2)
+        ax.tick_params(axis='y', labelsize=FONTSIZE-2)
+
+    # add legend that spans across all subplots, in one row, at the center for the subplots, and place it outside the plot 
+    f.legend(loc='upper center', bbox_to_anchor=(0.5, 1.1), ncol=6, fontsize=FONTSIZE-2, frameon=False, labels=[f'Type {task}' for task in tasks])
+    #axes[int(len(models)/2)].legend(fontsize=FONTSIZE-4, frameon=False,  loc="upper center", bbox_to_anchor=(.5, 1.2), ncol=6)  # place legend outside the plot
+    sns.despine()
+    f.tight_layout()
+    plt.show()
+    f.savefig(f'{SYS_PATH}/categorisation/figures/shepard1961_comparison.svg', bbox_inches='tight', dpi=300)

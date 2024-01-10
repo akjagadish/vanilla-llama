0e2c1a318b27b7c48635903c3f0e02ffa468ae31
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-01-10 21:53:43 +0100

    update model scripts

--- categorisation/shell-scripts/run_metalearner_simulation.sh
+++ categorisation/shell-scripts/run_metalearner_simulation.sh
@@ -4,7 +4,7 @@
 #SBATCH --job-name=simulate_metalearner
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=akshaykjagadish@gmail.com
-#SBATCH --time=72:00:00
+#SBATCH --time=24:00:00
 #SBATCH --cpus-per-task=18
 
 
@@ -25,7 +25,12 @@ export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 # python rl2/simulate.py --experiment johanssen_categorisation --num-runs 1 --beta 0.15 --model-name env=claude_generated_tasks_paramsNA_dim4_data650_tasks8950_pversion5_stage1_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
 # python rl2/simulate.py --experiment johanssen_categorisation --num-runs 1 --beta 0.25 --model-name env=claude_generated_tasks_paramsNA_dim4_data650_tasks8950_pversion5_stage1_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
 # python rl2/simulate.py --experiment johanssen_categorisation --num-runs 1 --beta 0.3 --model-name env=claude_generated_tasks_paramsNA_dim4_data650_tasks8950_pversion5_stage1_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
-# python rl2/simulate.py --experiment johanssen_categorisation --num-runs 1 --beta 0.4 --model-name env=claude_generated_tasks_paramsNA_dim4_data650_tasks8950_pversion5_stage1_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --experiment johanssen_categorisation --num-runs 1 --beta 0.4 --model-name env=dim4synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic 
+
+## run them in parallel using slurm
+# python rl2/simulate.py --job-id ${SLURM_ARRAY_TASK_ID} --experiment johanssen_categorisation --num-runs 5 --model-name env=claude_generated_tasks_paramsNA_dim4_data650_tasks8950_pversion5_stage1_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1
+# python rl2/simulate.py --job-id ${SLURM_ARRAY_TASK_ID} --experiment johanssen_categorisation --num-runs 1 --model-name env=dim4synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=0_synthetic 
+python rl2/simulate.py --job-id ${SLURM_ARRAY_TASK_ID} --experiment johanssen_categorisation --num-runs 1 --model-name env=dim4synthetic_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1_syntheticnonlinear
 
 ## smith task
 # python rl2/simulate.py --experiment smith_categorisation --num-runs 10 --beta 0.  --model-name env=claude_generated_tasks_paramsNA_dim6_data500_tasks12910_pversion5_stage2_model=transformer_num_episodes500000_num_hidden=256_lr0.0003_num_layers=6_d_model=64_num_head=8_noise0.0_shuffleTrue_run=1

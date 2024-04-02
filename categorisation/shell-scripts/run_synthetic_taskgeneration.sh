1c352589ea92dcb1fa21a865422470455bcf0d23
Akshay Jagadish <ajagadish@eris01.bc.rzg.mpg.de>
2024-04-02 13:57:36 +0200

    run analysis for rebuttals

--- categorisation/shell-scripts/run_synthetic_taskgeneration.sh
+++ categorisation/shell-scripts/run_synthetic_taskgeneration.sh
@@ -16,5 +16,7 @@ export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK}
 
 # python simulate_data.py --num-tasks 5000 --num-dims 3 --max-steps 100
 # python simulate_data.py --num-tasks 5000 --num-dims 3 --max-steps 100  --nonlinear
+# python simulate_data.py --num-tasks 1000 --num-dims 4 --max-steps 650  --nonlinear
+python simulate_data.py --num-tasks 1000 --num-dims 4 --max-steps 650  --nonlinear #4 layers, 128 hidden units
 # python simulate_data.py --num-tasks 10 --num-dims 3 --max-steps 100  --rmc
-python simulate_data.py --num-tasks 6518 --num-dims 3 --max-steps 600  --rmc --batch-size 100
+# python simulate_data.py --num-tasks 6518 --num-dims 3 --max-steps 600  --rmc --batch-size 100
